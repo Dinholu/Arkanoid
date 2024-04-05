c4728c28a8f1f3d3eac92435977d9cea73f00ff6
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2024-04-05 16:48:33 +0200

    FEAT - Collision OK 0 speed factor

--- arkanoid.c
+++ arkanoid.c
@@ -8,339 +8,306 @@
 #define BRICK_WIDTH 30
 #define BRICK_HEIGHT 13
 #define NUM_ROWS 15
+#define FIRST_LINE 1
 #define NUM_BRICKS (NUM_BRICKS_PER_ROW * NUM_ROWS)
-#define WHITE_BRICK {1,1,BRICK_WIDTH,BRICK_HEIGHT}
-#define ORANGE_BRICK {31,1,BRICK_WIDTH,BRICK_HEIGHT}
-#define BLUE1_BRICK {63,1,BRICK_WIDTH,BRICK_HEIGHT}
-#define GREEN1_BRICK {95,1,BRICK_WIDTH,BRICK_HEIGHT}
-#define BLUE2_BRICK {127,1,BRICK_WIDTH,BRICK_HEIGHT}
-#define GREEN2_BRICK {159,1,BRICK_WIDTH,BRICK_HEIGHT}
-
+#define WHITE_BRICK {FIRST_LINE,FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define ORANGE_BRICK {(FIRST_LINE * 32),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define BLUE1_BRICK {(FIRST_LINE * 64),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define GREEN1_BRICK {(FIRST_LINE * 96),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define BLUE2_BRICK {(FIRST_LINE * 128),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define GREEN2_BRICK {(FIRST_LINE * 156),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define RED_BRICK {FIRST_LINE,(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define BLUE3_BRICK {(FIRST_LINE * 32),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define PINK_BRICK {(FIRST_LINE * 64),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define YELLOW_BRICK {(FIRST_LINE * 96),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define RED2_BRICK {(FIRST_LINE * 128),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define BLUE4_BRICK {(FIRST_LINE * 156),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define GREY_BRICK {FIRST_LINE,(FIRST_LINE * 32),BRICK_WIDTH,BRICK_HEIGHT}
+#define S_BONUS {(FIRST_LINE * 256),FIRST_LINE,BRICK_WIDTH,BRICK_HEIGHT}
+#define C_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 16),BRICK_WIDTH,BRICK_HEIGHT}
+#define L_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 32),BRICK_WIDTH,BRICK_HEIGHT}
+#define E_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 48),BRICK_WIDTH,BRICK_HEIGHT}
+#define O_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 64),BRICK_WIDTH,BRICK_HEIGHT}
+#define D_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 80),BRICK_WIDTH,BRICK_HEIGHT}
+#define B_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 96),BRICK_WIDTH,BRICK_HEIGHT}
+#define P_BONUS {(FIRST_LINE * 256),(FIRST_LINE * 112),BRICK_WIDTH,BRICK_HEIGHT}
 
 const int FPS = 60.0;
-// struct { double x; double y; } ball_speed;
 
 struct
 {
-	double x;
-	double y;
-	double vx;
-	double vy;
+    double x;
+    double y;
+    double vx;
+    double vy;
 } ball;
 
 struct Brick
 {
-	double x;
-	double y;
-	int type;
-	bool isVisible;
+    double x;
+    double y;
+    int type;
+    bool isVisible;
 };
 
 struct Brick brick[NUM_BRICKS];
 
 bool ballIsAttached = false;
 Uint64 prev, now; // timers
-double delta_t;	  // durée frame en ms
+double delta_t;   // durée frame en ms
 int x_vault;
 
 SDL_Window *pWindow = NULL;
 SDL_Surface *win_surf = NULL;
 SDL_Surface *plancheSprites = NULL;
 SDL_Surface *gameSprites = NULL;
+SDL_Surface *asciiSprites = NULL;
 
 SDL_Rect srcBg = {0, 128, 96, 128}; // x,y, w,h (0,0) en haut a gauche
 SDL_Rect srcBall = {0, 96, 24, 24};
 SDL_Rect srcVaiss = {128, 0, 128, 32};
-SDL_Rect srcBrick = {1, 1, 30, 13};
+SDL_Rect srcBrick = C_BONUS;
 
 bool isCollision(SDL_Rect rect1, SDL_Rect rect2)
 {
-	return rect1.x < rect2.x + rect2.w &&
-		   rect1.x + rect1.w > rect2.x &&
-		   rect1.y < rect2.y + rect2.h &&
-		   rect1.y + rect1.h > rect2.y;
+    return rect1.x < rect2.x + rect2.w &&
+           rect1.x + rect1.w > rect2.x &&
+           rect1.y < rect2.y + rect2.h &&
+           rect1.y + rect1.h > rect2.y;
 }
 
 bool allBricksInvisible()
 {
-	for (int i = 0; i < NUM_BRICKS; i++)
-	{
-		if (brick[i].isVisible)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-void handleCollisions()
-{
-    SDL_Rect ballRect = {ball.x, ball.y, 24, 24};
     for (int i = 0; i < NUM_BRICKS; i++)
     {
         if (brick[i].isVisible)
         {
-            SDL_Rect brickRect = {brick[i].x, brick[i].y, 30, 13};
+            return false;
+        }
+    }
+    return true;
+}
+
+void handleCollisions()
+{
+    // Ball collision with walls
+    if ((ball.x < 1) || (ball.x > (win_surf->w - 25)))
+        ball.vx *= -1;
+    if ((ball.y < 1) || (ball.y > (win_surf->h - 25)))
+        ball.vy *= -1;
+
+    // Ball collision with vault
+    if ((ball.y + 24 > win_surf->h - 32) && (ball.x + 24 > x_vault) && (ball.x < x_vault + 128))
+    {
+        double relativeCollisionX = (ball.x + 12) - (x_vault + 64);
+        double normalizedRelativeCollisionX = relativeCollisionX / 64.0;
+
+        double bounceAngle = normalizedRelativeCollisionX * M_PI / 3.0;
+        double speed = sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+
+        ball.vx = speed * sin(bounceAngle);
+        ball.vy = -speed * cos(bounceAngle);
+    }
+
+    // Ball collision with bottom boundary
+    if (ball.y > (win_surf->h - 25))
+    {
+        ball.x = x_vault + 52;
+        ball.y = win_surf->h - 58;
+        ball.vy = 0;
+        ball.vx = 0;
+        ballIsAttached = true;
+    }
+
+    // Ball collision with bricks
+    for (int i = 0; i < NUM_BRICKS; i++)
+    {
+        if (brick[i].isVisible)
+        {
+            SDL_Rect ballRect = {ball.x, ball.y, 24, 24};
+            SDL_Rect brickRect = {brick[i].x, brick[i].y, BRICK_WIDTH, BRICK_HEIGHT};
+
             if (isCollision(ballRect, brickRect))
             {
                 brick[i].isVisible = false;
 
-                // Calcul de la position relative de la collision
-                double relativeCollisionX = (ball.x + 12) - (brick[i].x + 15);
-                double relativeCollisionY = (ball.y + 12) - (brick[i].y + 6);
+                // Adjust ball velocity after collision with a brick
+                double ballCenterX = ball.x + 12;
+                double ballCenterY = ball.y + 12;
 
-                // Calcul de l'angle de rebond
-                double bounceAngle = 0.0;
-                if (relativeCollisionX >= 0 && relativeCollisionY >= 0) {
-                    // Premier cadran
-                    if (relativeCollisionX > relativeCollisionY) {
-                        bounceAngle = -M_PI / 3.0; // Haut gauche
-                    } else {
-                        bounceAngle = M_PI / 3.0; // Bas droit
-                    }
-                } else if (relativeCollisionX >= 0 && relativeCollisionY < 0) {
-                    // Deuxième cadran
-                    if (relativeCollisionX > -relativeCollisionY) {
-                        bounceAngle = -M_PI / 3.0; // Haut gauche
-                    } else {
-                        bounceAngle = -M_PI / 3.0; // Bas gauche
-                    }
-                } else if (relativeCollisionX < 0 && relativeCollisionY < 0) {
-                    // Troisième cadran
-                    if (-relativeCollisionX > -relativeCollisionY) {
-                        bounceAngle = M_PI / 3.0; // Bas droit
-                    } else {
-                        bounceAngle = -M_PI / 3.0; // Haut gauche
-                    }
-                } else {
-                    // Quatrième cadran
-                    if (-relativeCollisionX > relativeCollisionY) {
-                        bounceAngle = M_PI / 3.0; // Bas gauche
-                    } else {
-                        bounceAngle = -M_PI / 3.0; // Haut droit
-                    }
-                }
+                double brickCenterX = brick[i].x + (BRICK_WIDTH / 2);
+                double brickCenterY = brick[i].y + (BRICK_HEIGHT / 2);
 
-				if (relativeCollisionY > 0 && fabs(relativeCollisionY) > fabs(relativeCollisionX)) {
-                    bounceAngle *= -1;
-					break;
-                }
+                double dx = ballCenterX - brickCenterX;
+                double dy = ballCenterY - brickCenterY;
 
-                // Ajuster la vitesse de la balle
+                double reflectionAngle = atan2(dy, dx);
                 double speed = sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
-                ball.vx = speed * sin(bounceAngle);
-                ball.vy = -speed * cos(bounceAngle);
 
-                const double ACCELERATION_FACTOR = 1.1;
-                ball.vx *= ACCELERATION_FACTOR;
-                ball.vy *= ACCELERATION_FACTOR;
-                break;
+                ball.vx = speed * cos(reflectionAngle);
+                ball.vy = speed * sin(reflectionAngle);
+
+                break; // Exit loop after handling collision with one brick
             }
         }
     }
 }
 
 
-
 void loadLevelFromFile(const char *filename)
 {
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
 
-	int row = 0;
-	int col = 0;
-	while (fscanf(file, "%1d", &brick[row * NUM_BRICKS_PER_ROW + col].type) == 1)
-	{
-		brick[row * NUM_BRICKS_PER_ROW + col].x = col * BRICK_WIDTH;
-		brick[row * NUM_BRICKS_PER_ROW + col].y = row * BRICK_HEIGHT;
-		brick[row * NUM_BRICKS_PER_ROW + col].isVisible = brick[row * NUM_BRICKS_PER_ROW + col].type; // Mettez à jour en fonction de la valeur lue
+    int row = 0;
+    int col = 0;
+    while (fscanf(file, "%1d", &brick[row * NUM_BRICKS_PER_ROW + col].type) == 1)
+    {
+        brick[row * NUM_BRICKS_PER_ROW + col].x = col * BRICK_WIDTH;
+        brick[row * NUM_BRICKS_PER_ROW + col].y = row * BRICK_HEIGHT;
+        brick[row * NUM_BRICKS_PER_ROW + col].isVisible = brick[row * NUM_BRICKS_PER_ROW + col].type;
 
-		printf("Loaded brick at (%f, %f) - Type: %d\n", brick[row * NUM_BRICKS_PER_ROW + col].x, brick[row * NUM_BRICKS_PER_ROW + col].y, brick[row * NUM_BRICKS_PER_ROW + col].type);
+        printf("Loaded brick at (%f, %f) - Type: %d\n", brick[row * NUM_BRICKS_PER_ROW + col].x, brick[row * NUM_BRICKS_PER_ROW + col].y, brick[row * NUM_BRICKS_PER_ROW + col].type);
 
-		col++;
-		if (col == NUM_BRICKS_PER_ROW)
-		{
-			col = 0;
-			row++;
-			if (row == NUM_ROWS)
-			{
-				fclose(file);
-				return;
-			}
-		}
-	}
+        col++;
+        if (col == NUM_BRICKS_PER_ROW)
+        {
+            col = 0;
+            row++;
+            if (row == NUM_ROWS)
+            {
+                fclose(file);
+                return;
+            }
+        }
+    }
 
-	fclose(file);
+    fclose(file);
 }
 
 void init()
 {
-	pWindow = SDL_CreateWindow("Arknoid", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
-	win_surf = SDL_GetWindowSurface(pWindow);
-	plancheSprites = SDL_LoadBMP("./sprites.bmp");
-	gameSprites = SDL_LoadBMP("./Arkanoid_sprites.bmp");
+    pWindow = SDL_CreateWindow("Arknoid", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+    win_surf = SDL_GetWindowSurface(pWindow);
+    plancheSprites = SDL_LoadBMP("./sprites.bmp");
+    gameSprites = SDL_LoadBMP("./Arkanoid_sprites.bmp");
+    asciiSprites = SDL_LoadBMP("./Arkanoid_ascii.bmp");
 
-	SDL_SetColorKey(plancheSprites, true, 0); // 0: 00/00/00 noir -> transparent
-	SDL_SetColorKey(gameSprites, true, 0);	  // 0: 00/00/00 noir -> transparent
+    SDL_SetColorKey(plancheSprites, true, 0); // 0: 00/00/00 noir -> transparent
+    SDL_SetColorKey(gameSprites, true, 0);    // 0: 00/00/00 noir -> transparent
 
-	ball.x = win_surf->w / 2;
-	ball.y = win_surf->h / 2;
-	ball.vx = 2.0;
-	ball.vy = 2.8;
+    ball.x = win_surf->w / 2;
+    ball.y = win_surf->h / 2;
+    ball.vx = 2.0;
+    ball.vy = 2.8;
 
-	loadLevelFromFile("level1.txt");
-	now = SDL_GetPerformanceCounter();
+    loadLevelFromFile("level1.txt");
+    now = SDL_GetPerformanceCounter();
 }
 
-// fonction qui met à jour la surface de la fenetre "win_surf"
 void draw()
 {
-	// remplit le fond
-	SDL_Rect dest = {0, 0, 0, 0};
-	for (int j = 0; j < win_surf->h; j += 128)
-		for (int i = 0; i < win_surf->w; i += 96)
-		{
-			dest.x = i;
-			dest.y = j;
-			SDL_BlitSurface(plancheSprites, &srcBg, win_surf, &dest);
-		}
+    SDL_Rect dest = {0, 0, 0, 0};
+    for (int j = 0; j < win_surf->h; j += 128)
+        for (int i = 0; i < win_surf->w; i += 96)
+        {
+            dest.x = i;
+            dest.y = j;
+            SDL_BlitSurface(plancheSprites, &srcBg, win_surf, &dest);
+        }
 
-	// affiche balle
-	SDL_Rect dstBall = {ball.x, ball.y, 0, 0};
-	SDL_BlitSurface(plancheSprites, &srcBall, win_surf, &dstBall);
+    SDL_Rect dstBall = {ball.x, ball.y, 0, 0};
+    SDL_BlitSurface(plancheSprites, &srcBall, win_surf, &dstBall);
 
-	// dedplacement
-	ball.x += ball.vx; // / delta_t;
-	ball.y += ball.vy; // / delta_t;
+    ball.x += ball.vx; // / delta_t;
+    ball.y += ball.vy; // / delta_t;
 
-	// collision bord
-	if ((ball.x < 1) || (ball.x > (win_surf->w - 25)))
-		ball.vx *= -1;
-	if ((ball.y < 1) || (ball.y > (win_surf->h - 25)))
-		ball.vy *= -1;
+    handleCollisions(); // Handle all collisions
 
-	// Collision avec le vaisseau
-	if (
-		(ball.y + 24 > win_surf->h - 32) &&
-		(ball.x + 24 > x_vault) &&
-		(ball.x < x_vault + 128) &&
-		(ball.y < win_surf->h - 32))
-	{
-		// La balle a collisionné avec le vaisseau
-		// Calculez l'angle de rebond en fonction de la position de la collision
-		double relativeCollisionX = (ball.x + 12) - (x_vault + 64);		 // Position relative de la collision par rapport au centre du vaisseau
-		double normalizedRelativeCollisionX = relativeCollisionX / 64.0; // Normalisez la position relative
+    dest.x = x_vault;
+    dest.y = win_surf->h - 32;
+    SDL_BlitSurface(plancheSprites, &srcVaiss, win_surf, &dest);
 
-		// Ajustez l'angle de rebond en fonction de la position relative
-		double bounceAngle = normalizedRelativeCollisionX * M_PI / 3.0; // Utilisez un angle de rebond entre -pi/3 et pi/3
-		double speed = sqrt(ball.vx * ball.vx + ball.vy * ball.vy);		// Calculez la vitesse actuelle de la balle
+    if (ballIsAttached)
+    {
+        ball.x = x_vault + 52;
+        ball.y = win_surf->h - 58;
+    }
 
-		// Mettez à jour les composantes de la vitesse en fonction de l'angle de rebond
-		ball.vx = speed * sin(bounceAngle);
-		ball.vy = -speed * cos(bounceAngle);
-	}
-
-	// touche bas -> rouge
-	if (ball.y > (win_surf->h - 25))
-	{
-		ball.x = x_vault + 52;	   // 52 est la moitié de la largeur du vaisseau
-		ball.y = win_surf->h - 58; // 58 est la hauteur de la balle + la hauteur du vaisseau
-		ball.vy = 0;
-		ball.vx = 0;
-		ballIsAttached = true;
-		// srcBall.y = 64;
-	}
-	// touche bas -> vert
-	if (ball.y < 1)
-		srcBall.y = 96;
-
-	// vaisseau
-	dest.x = x_vault;
-	dest.y = win_surf->h - 32;
-	SDL_BlitSurface(plancheSprites, &srcVaiss, win_surf, &dest);
-
-	// Si la balle est attachée, la positionner sur le vaisseau
-	if (ballIsAttached)
-	{
-		ball.x = x_vault + 52;
-		ball.y = win_surf->h - 58;
-	}
-
-	// Brique
-	for (int i = 0; i < NUM_BRICKS; i++)
-	{
-		if (brick[i].isVisible)
-		{
-			SDL_Rect dstBrick = {brick[i].x, brick[i].y, 0, 0};
-			SDL_BlitSurface(gameSprites, &srcBrick, win_surf, &dstBrick);
-		}
-	}
+    for (int i = 0; i < NUM_BRICKS; i++)
+    {
+        if (brick[i].isVisible)
+        {
+            SDL_Rect dstBrick = {brick[i].x, brick[i].y, 0, 0};
+            SDL_BlitSurface(gameSprites, &srcBrick, win_surf, &dstBrick);
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
-	{
-		return 1;
-	}
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+    {
+        return 1;
+    }
 
-	init();
+    init();
 
-	bool quit = false;
-	SDL_Event event; // Déclaration de la variable event ici
+    bool quit = false;
+    SDL_Event event;
 
-	while (!quit)
-	{
-		if (allBricksInvisible())
-		{
-			printf("Congratulations! You've won the game!\n");
-			quit = true; // End the game loop
-		}
-		SDL_PumpEvents();
-		const Uint8 *keys = SDL_GetKeyboardState(NULL);
-		// Lancer le jeu avec la touche "Space"
-		if (keys[SDL_SCANCODE_SPACE] && ball.vy == 0)
-		{
-			// Lancer le jeu (assigner une vitesse initiale à la balle)
-			ballIsAttached = false;
-			ball.vy = -1.4; // Vitesse vers le haut
-			ball.vx = -1.0;
-		}
-		handleCollisions();
+    while (!quit)
+    {
+        if (allBricksInvisible())
+        {
+            printf("Congratulations! You've won the game!\n");
+            quit = true;
+        }
+        SDL_PumpEvents();
+        const Uint8 *keys = SDL_GetKeyboardState(NULL);
+        if (keys[SDL_SCANCODE_SPACE] && ball.vy == 0)
+        {
+            ballIsAttached = false;
+            ball.vy = -1.4;
+            ball.vx = -1.0;
+        }
 
-		if (keys[SDL_SCANCODE_LEFT])
-			x_vault -= 10;
-		if (keys[SDL_SCANCODE_RIGHT])
-			x_vault += 10;
-		if (keys[SDL_SCANCODE_ESCAPE])
-			quit = true;
+        if (keys[SDL_SCANCODE_LEFT])
+            x_vault -= 10;
+        if (keys[SDL_SCANCODE_RIGHT])
+            x_vault += 10;
+        if (keys[SDL_SCANCODE_ESCAPE])
+            quit = true;
 
-		if (x_vault < 0)
-			x_vault = 0;
-		if (x_vault > win_surf->w - 128) // 128 est la largeur du vaisseau
-			x_vault = win_surf->w - 128;
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-			{
-				quit = true; // Quitte la boucle principale si l'événement de fermeture de la fenêtre est détecté
-			}
-		}
-		draw();
-		SDL_UpdateWindowSurface(pWindow);
-		now = SDL_GetPerformanceCounter();
-		delta_t = 1.0 / FPS - (double)(now - prev) / (double)SDL_GetPerformanceFrequency();
-		prev = now;
-		if (delta_t > 0)
-			SDL_Delay((Uint32)(delta_t * 1000));
-		// printf("dt = %lf\n", delta_t * 1000);
-		prev = SDL_GetPerformanceCounter();
-	}
+        if (x_vault < 0)
+            x_vault = 0;
+        if (x_vault > win_surf->w - 128)
+            x_vault = win_surf->w - 128;
 
-	SDL_Quit();
-	return 0;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        draw();
+
+        SDL_UpdateWindowSurface(pWindow);
+        now = SDL_GetPerformanceCounter();
+        delta_t = 1.0 / FPS - (double)(now - prev) / (double)SDL_GetPerformanceFrequency();
+        prev = now;
+
+        if (delta_t > 0)
+            SDL_Delay((Uint32)(delta_t * 1000));
+        prev = SDL_GetPerformanceCounter();
+    }
+
+    SDL_Quit();
+    return 0;
 }
