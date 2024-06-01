f703a1ad913ee072f86c67a957a5b6bcb27a34b9
Alizee <alizeehett@hotmail.fr>
2024-06-01 15:18:59 +0200

    Libere espace

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <SDL2/SDL.h>
 #include "game.h"
 
+void freeBricks()
+{
+    if (brick != NULL)
+    {
+        free(brick);
+        brick = NULL;
+    }
+}
+void freeAllResources()
+{
+    // Libérer toutes les surfaces SDL
+    if (gameSprites)
+        SDL_FreeSurface(gameSprites);
+    if (asciiSprites)
+        SDL_FreeSurface(asciiSprites);
+    if (menuSprites)
+        SDL_FreeSurface(menuSprites);
+    if (topWallSprites)
+        SDL_FreeSurface(topWallSprites);
+    if (leftWallSprites)
+        SDL_FreeSurface(leftWallSprites);
+    if (rightWallSprites)
+        SDL_FreeSurface(rightWallSprites);
+    freeBricks();
+
+    // Détruire la fenêtre
+    if (pWindow)
+        SDL_DestroyWindow(pWindow);
+
+    // Quitter SDL
+    SDL_Quit();
+}
 int main(int argc, char **argv)
 {
     initializeSDL();
@@ -8,6 +40,6 @@ int main(int argc, char **argv)
     resetGame();
     mainGameLoop();
 
-    SDL_Quit();
+    freeAllResources();
     return 0;
 }
